0b6d6cab384ded5f23bb621a7ef15ec060c0bd50
hyeonsu <trevor0910@sungkyul.ac.kr>
2023-11-09 21:33:00 +0900

    miniblog

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,6 +16,7 @@ import Signup from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
 import DarkOrLight from './chapter_14/DarkOrLight';
+import Blocks from './chapter_15/Blocks';
 
 
 
